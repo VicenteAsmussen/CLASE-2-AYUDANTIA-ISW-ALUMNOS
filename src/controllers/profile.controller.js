71fcec79b38200f3915e95689e50acdf3ce02355
VicenteAsmussen <vicente.asmussen2201@alumnos.ubiobio.cl>
2025-10-02 15:58:01 -0300

    Creacion patch y delete
    Se creo las funciones Patch y Delete dentro del backen, para la tarea se creo el .env y se modifico con los datos de mi base de datos, luego se modifico profile.controller.js el cual recibe las solicitudes de delete o patch para enviarlo a user.service.js el cual igual se modifico el cual realiza las operaciones segun la solicitud recibida y valida lo ingresado

--- src/controllers/profile.controller.js
+++ src/controllers/profile.controller.js
@@ -1,4 +1,7 @@
-import { handleSuccess } from "../Handlers/responseHandlers.js";
+//import { handleSuccess } from "../Handlers/responseHandlers.js";
+import { handleSuccess, handleErrorClient } from "../Handlers/responseHandlers.js";
+import { updateUserProfile, deleteUserAccount } from "../services/user.service.js"; 
+
 
 export function getPublicProfile(req, res) {
   handleSuccess(res, 200, "Perfil público obtenido exitosamente", {
@@ -13,4 +16,35 @@ export function getPrivateProfile(req, res) {
     message: `¡Hola, ${user.email}! Este es tu perfil privado. Solo tú puedes verlo.`,
     userData: user,
   });
+}
+
+//funcion patch
+export async function updateProfile(req, res) {
+  try {
+    const userId = req.user.id;             // id desde el token JWT
+    const { email, password } = req.body;   // actualizar
+
+    const updatedUser = await updateUserProfile(userId, { email, password });
+
+    handleSuccess(res, 200, "Usuario actualizado correctamente", {
+      id: updatedUser.id,
+      email: updatedUser.email
+    });
+
+  } catch (error) {
+    handleErrorClient(res, 400, error.message);
+  }
+}
+
+export async function deleteProfile(req, res) {
+  try {
+    const userId = req.user.id;             // Id del token
+
+    await deleteUserAccount(userId);        // funcion eliminar
+
+    handleSuccess(res, 200, "Cuenta eliminada correctamente", {});
+
+  } catch (error) {
+    handleErrorClient(res, 400, error.message);
+  }
 }
--- src/services/user.service.js
+++ src/services/user.service.js
@@ -1,4 +1,4 @@
-import { AppDataSource } from "../config/configDB.js";
+import { AppDataSource } from "../config/configDb.js";
 import { User } from "../entities/user.entity.js";
 import bcrypt from "bcrypt";
 
@@ -17,4 +17,37 @@ export async function createUser(data) {
 
 export async function findUserByEmail(email) {
   return await userRepository.findOneBy({ email });
+}
+//funcion patch
+export async function updateUserProfile(userId, { email, password }) {
+  //Buscar usuario
+  const user = await userRepository.findOneBy({ id: userId });
+  if (!user) throw new Error("Usuario no encontrado");
+
+  //cambiar email
+  if (email) {
+    const existingUser = await userRepository.findOneBy({ email });
+    if (existingUser && existingUser.id !== userId) {
+      throw new Error("El email ya está en uso");
+    }
+    user.email = email;
+  }
+
+  //cambiar contraseña
+  if (password) {
+    user.password = await bcrypt.hash(password, 10);
+  }
+
+  //guardar
+  return await userRepository.save(user);
+}
+
+//funcion eliminar
+export async function deleteUserAccount(userId) {
+  //Buscar usuario
+  const user = await userRepository.findOneBy({ id: userId });
+  if (!user) throw new Error("Usuario no encontrado");
+
+  //Eliminar usuario
+  await userRepository.remove(user);
 }
